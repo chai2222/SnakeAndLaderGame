7b34b2e65143b1dd18130eb315dff3e5dc582660
chai2222 <chaitanayajagadale6@gmail.com>
2022-12-30 11:32:16 +0530

    GameFlow

---
+++ SnakeLader.cs
@@ -0,0 +1,14 @@
+﻿
+
+namespace SnakeAndLader
+{
+    public static class SnakeLader
+    {
+        public static void displayData()
+        {
+            int start_Position = 0;
+            int player1;
+            Console.WriteLine("Initial Position Of Player1 is :" + start_Position);
+        }
+    }
+}
