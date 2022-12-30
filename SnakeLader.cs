a0e92871c4504de2d2bcb93cb39a000a46f8df13
chai2222 <chaitanayajagadale6@gmail.com>
2022-12-30 11:57:57 +0530

    GetNumber1to6

--- SnakeLader.cs
+++ SnakeLader.cs
@@ -1,14 +1,25 @@
 ﻿
 
+using System.Runtime.ExceptionServices;
+
 namespace SnakeAndLader
 {
     public static class SnakeLader
     {
         public static void displayData()
         {
-            int start_Position = 0;
+            int Position = 0;
             int player1;
-            Console.WriteLine("Initial Position Of Player1 is :" + start_Position);
+            int die_Roll = 0;
+            Console.WriteLine("Initila Position Of Player1 is: " + Position);
+            Random random = new Random();  
+            die_Roll = random.Next(1, 7);
+
+            Console.WriteLine("Die_Roll: " + die_Roll);
+
+            Position = die_Roll;
+
+            Console.WriteLine("After The Die_Roll Player1 Position is: " + Position);
         }
     }
 }
