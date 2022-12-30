31981891df589d3704a5539a90daf6cc5bdbf649
chai2222 <chaitanayajagadale6@gmail.com>
2022-12-30 11:25:53 +0530

    Greeting Massage

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿namespace SnakeAndLader
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome To SnakeAndLader Game");
+            SnakeLader.displayData();
+        }
+
+    }
+
+}
